1ccad48a503d4b22d4daeecf4285c481ac7b0f93
LurchingDart <lurchdev@gmail.com>
2024-10-31 12:39:15 -0600

    Halloween Partyyyyy or im

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://www.inferno.wtf/",
   integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
