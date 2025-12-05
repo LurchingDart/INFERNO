b599db521b155a1b2ec8df389888d5bf42b3af85
LurchingDart <lurchdev@gmail.com>
2025-12-04 23:42:23 -0600

    chore: update fonts and splash h1 size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+        display: ["BootzyTM", "cursive"],
       },
       fontSize: {
         sm: '0.8rem',
