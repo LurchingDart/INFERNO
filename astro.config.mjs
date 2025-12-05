1df7c911d8dfc027e9794467ad83324cdd9554dc
LurchingDart <lurchdev@gmail.com>
2025-12-05 01:36:38 -0600

    chore: remove unused icon integration and font dependencies, delete background image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.inferno.wtf/",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
   output: "server",
   adapter: netlify(),
 });
