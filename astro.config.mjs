221f56a79f850e8de84fde269947ef2722ae69a5
LurchingDart <lurchdev@gmail.com>
2025-12-05 01:41:22 -0600

    chore: remove Netlify adapter from Astro config and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify(),
 });
